c19ca0e01a51ca414841b688942b9e66a8a7c3a0
FFreitas <francisco.freitas.ff@gmail.com>
2024-08-12 17:23:21 +0100

    customer microservice updated

--- services/customer/src/main/java/com/ffreitas/ecommerce/customer/CustomerController.java
+++ services/customer/src/main/java/com/ffreitas/ecommerce/customer/CustomerController.java
@@ -4,14 +4,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("api/v1/customers")
+@RequestMapping("/api/v1/customers")
 @RequiredArgsConstructor
 public class CustomerController {
 
